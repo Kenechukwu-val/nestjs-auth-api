5d79ff92661e587c60bf3234d17285f2c1543930
Nwafor Valentine Kenechukwu <nwaforvalentinekenechukwu@Nwafors-MacBook-Pro.local>
2026-06-23 13:16:54 +0100

    Chore: updated the auth DTOs

--- src/auth/dto/resend-verification-email.dto.ts
+++ src/auth/dto/resend-verification-email.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsEmail } from 'class-validator';
+import { IsEmail, Matches } from 'class-validator';
 
 export class ResendVerificationEmailDto {
   @ApiProperty({ example: 'test@example.com' })
